f3dbbc5346ed8325a3dcf61bc7e063e269710e62
ismontana <ismaotsss@gmail.com>
2025-03-13 23:45:29 -0600

    15 Set up web sockets in the backend

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -16,6 +16,12 @@ SITE_ID = 1
 
 WEBSITE_URL = 'http://localhost:8000'
 
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
@@ -55,6 +61,7 @@ REST_AUTH = {
 }
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,6 +116,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangobnb_backend.wsgi.application'
+ASGI_APPLICATION = 'djangobnb_backend.asgi.application'
 
 DATABASES = {
     'default': {
