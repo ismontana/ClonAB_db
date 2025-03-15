1f2ceb4d6d693f97f2f22a4b84088091662cd792
ismontana <ismaotsss@gmail.com>
2025-03-14 19:32:34 -0600

    17 Send/recive messages

--- djangobnb_backend/chat/consumers.py
+++ djangobnb_backend/chat/consumers.py
@@ -1,8 +1,6 @@
 import json
-
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-
 from .models import ConversationMessage
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -17,8 +15,34 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-    async def disconnect(self):
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+
+        conversation_id = data['data']['conversation_id']
+        sent_to_id = data['data']['sent_to_id']
+        name = data['data']['name']
+        body = data['data']['body']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'body': body,
+                'name': name
+            }
+        )
+
+    async def chat_message(self, event):
+        body = event['body']
+        name = event['name']
+
+        await self.send(text_data=json.dumps({
+            'body': body,
+            'name': name
+        }))
